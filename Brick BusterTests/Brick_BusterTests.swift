278f252adbcb0484ca71a1ea6df45307f37e4bb7
Cheng Peng <cheng@satorianalytics.com>
2020-10-16 01:36:15 -0400

    Initial Commit

---
+++ Brick BusterTests/Brick_BusterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Brick_BusterTests.swift
+//  Brick BusterTests
+//
+//  Created by yujian on 2020/10/16.
+//  Copyright © 2020 tra. All rights reserved.
+//
+
+import XCTest
+@testable import Brick_Buster
+
+class Brick_BusterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
